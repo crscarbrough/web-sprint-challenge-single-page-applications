a38fc8af293b94e0c9fc26e050cc6cacb01ee358
Claudia Scarbrough <rubyharrts@gmail.com>
2021-12-16 21:17:08 -0600

    Started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
+    <div className="App">
+      <header>
+        <h1>Pizza Pizza</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/">Order Pizza!</Link>
+        </nav>
+      </header>
       <p>You can remove this code and create your own header</p>
-    </>
+    </div>
   );
 };
 export default App;
